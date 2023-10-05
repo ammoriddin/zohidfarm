4f5660aa90de9f90773ba2be05167d09234af684
Ammoriddin <hakimjavoh@gmail.com>
2023-10-05 18:07:04 +0500

    updated

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -31,13 +31,13 @@ export default function Navbar() {
 
     return (
         <>
-            <nav className='w-full bg-[#f5f5f5]'>
+            <nav className='w-full bg-[#f5f5f5] '>
                 <div className='container py-5 relative'>
-                    {/* <div className={`bottom-0 left-0 fixed bg-black w-full h-[100vh] z-20 opacity-[0.6] ${xbutton}`}></div> */}
+                    <div className={`bottom-0 left-0 fixed bg-black w-full h-[100vh] z-20 opacity-[0.6] ${xbutton}`}></div>
                     <button onClick={burgermenu} className={`${burgerbutton} ml-auto block xl:hidden`}>
                         <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 18h16M4 12h16M4 6h16" stroke="#000" strokeWidth="2" strokeLinecap="round"/></svg>
                     </button>
-                    <ul ref={navref} className={`gap-5 justify-between right-[-40rem] fixed z-[999000] bottom-0 h-[100vh] xl:right-0 transition-all xl:h-[100%] xl:relative px-6 xl:p-0 text-[18px] font-[600] bg-[#fff]  xl:bg-transparent w-[70%] flex-col md:w-[50%] xl:w-[100%] xl:flex`}>
+                    <div ref={navref} className={`gap-5 justify-between right-[-40rem] fixed z-[999000] bottom-0 h-[100vh] xl:right-0 transition-all xl:h-[100%] xl:relative px-6 xl:p-0 text-[18px] font-[600] bg-[#fff]  xl:bg-transparent w-[70%] flex-col md:w-[50%] xl:w-[100%] xl:flex`}>
                         <div className='w-[10px] relative right-10 top-6  xl:translate-x-0 z-[9999999999999999] ml-auto block xl:hidden'>
                             <button onClick={xmenu} className={`${xbutton} w-full absolute`}>
                                 <svg xmlns="http://www.w3.org/2000/svg" xmlSpace="preserve" width="20" height="20" viewBox="0 0 460.775 460.775"><path d="M285.08 230.397 456.218 59.27c6.076-6.077 6.076-15.911 0-21.986L423.511 4.565a15.55 15.55 0 0 0-21.985 0l-171.138 171.14L59.25 4.565a15.551 15.551 0 0 0-21.985 0L4.558 37.284c-6.077 6.075-6.077 15.909 0 21.986l171.138 171.128L4.575 401.505c-6.074 6.077-6.074 15.911 0 21.986l32.709 32.719a15.555 15.555 0 0 0 21.986 0l171.117-171.12 171.118 171.12a15.551 15.551 0 0 0 21.985 0l32.709-32.719c6.074-6.075 6.074-15.909 0-21.986L285.08 230.397z"/></svg>
@@ -52,7 +52,7 @@ export default function Navbar() {
                             <Link onClick={() => {xmenu()}} to="about-we" spy={true} smooth={true} offset={50} duration={500} className=' my-1 hover:text-[#000] text-[#2f2f2fb8] transition-colors xl:my-0'><a href="">{t('navbar.aboutwe')}</a></Link>
                             <Link onClick={() => {xmenu()}} to="contact" spy={true} smooth={true} offset={50} duration={500} className=' my-1 hover:text-[#000] text-[#2f2f2fb8] transition-colors'><a href="">{t('contact')}</a></Link>
                         </div>
-                    </ul>
+                    </div>
                 </div>  
             </nav>
 
