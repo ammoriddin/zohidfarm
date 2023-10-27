e7a80bc5fe1ad0df10e095eebd546cef1069b52f
Ammoriddin <hakimjavoh@gmail.com>
2023-10-27 11:39:14 +0500

    initial commit

--- src/components/fotogalereya.jsx
+++ src/components/fotogalereya.jsx
@@ -14,7 +14,7 @@ export default function FotoGalereya() {
   const [lightboxDisplay, setLightBoxDisplay] = useState(false);
 
   const imageCards = images.map((image) => (
-    <img className="h-[100%] rounded-xl w-[100%] object-cover object-top" onClick={() => showImage(image)} src={image} />
+    <img className="h-[100%] rounded-xl w-[100%] object-cover object-center" onClick={() => showImage(image)} src={image} />
   ));
 
   const showImage = (image) => {
@@ -56,26 +56,11 @@ export default function FotoGalereya() {
         <div className='container'>
             <h1 className='text-[22px] sm:text-[25px] md:text-[30px] lg:text-[45px] mb-10 font-[600] text-center text-[#121212] w-[60%] block mx-auto'>{t("photos.h1")}</h1>
             <div className='flex flex-wrap gap-2 w-full items-center'>
-                <div className='w-[100%] flex flex-wrap md:w-[32%] gap-2'>
-                    <div className='w-[100%] h-[350px]'>
+                <div className='w-[100%] flex justify-center flex-wrap'>
+                    <div className='w-[50%] h-[650px]'>
                         {imageCards[0]}
                     </div>
-                    <div className='w-[100%] h-[350px]'>
-                        {imageCards[1]}
-                    </div>
                 </div>
-                <div className='w-[100%] h-[94vh] md:w-[32%] flex-shrink-[5]'>
-                    {imageCards[2]}
-                </div>
-                <div className='w-[100%] flex flex-wrap md:w-[32%] gap-2'>
-                    <div className='w-[100%] h-[350px]'>
-                        {imageCards[3]}
-                    </div>
-                    <div className='w-[100%] h-[350px]'>
-                        {imageCards[4]}
-                    </div>
-                </div>
-
             </div>
             {
             lightboxDisplay ? 
@@ -90,19 +75,6 @@ export default function FotoGalereya() {
             </div>
         : ""
         }
-            {/* <div className='flex flex-wrap gap-2 w-full'>
-                <div className='w-[100%] h-[300px] flex flex-wrap md:w-[32%] gap-2'>
-                    <img className='object-cover max-w-[47.5%] sm:max-w-[48%] md:max-w-[100%] rounded-xl' src={gallery2} alt="" />
-                    <img className='object-cover max-w-[47.5%] sm:max-w-[48%] md:max-w-[100%] rounded-xl' src={gallery3} alt="" />
-                </div>
-                <div className='w-[100%] h-[600px] md:w-[32%] flex-shrink-[5]'>
-                    <img className='w-[100%] object-cover rounded-xl' src={gallery1} alt="" />
-                </div>
-                <div className='w-[100%] h-[300px] flex flex-wrap md:w-[32%] gap-2'>
-                    <img className='object-cover max-w-[47.5%] sm:max-w-[48%] md:max-w-[100%] rounded-xl' src={gallery4} alt="" />
-                    <img className='object-cover max-w-[47.5%] sm:max-w-[48%] md:max-w-[100%] rounded-xl' src={gallery5} alt="" />
-                </div>
-            </div> */}
         </div>
     </div>
   )
